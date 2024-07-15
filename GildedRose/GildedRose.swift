6c71b397ce9561bfe11857b11873973240a00837
Mahdi Mahjoobi <mahdi.mahjoobi@impalastudios.com>
2024-07-15 16:41:52 +0200

    Done for today

--- GildedRose/GildedRose.swift
+++ GildedRose/GildedRose.swift
@@ -2,50 +2,50 @@ public class GildedRose {
     public static func updateQuality(_ items: Array<Item>) -> [Item] {
         var items = items
         for i in 0 ..< items.count {
-            if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-                if items[i].quality > 0 {
-                    if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                        items[i].quality = items[i].quality - 1
-                    }
+            let itemName = ItemName(rawValue: items[i].name)
+            
+            switch itemName {
+            case .agedBrie:
+                items[i].addQuantity()
+                
+                items[i].sellIn = items[i].sellIn - 1
+                
+                if items[i].sellIn < 0 {
+                    items[i].addQuantity()
                 }
-            } else {
+            case .TAFKAL80ETC:
                 if items[i].quality < 50 {
                     items[i].quality = items[i].quality + 1
                     
-                    if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-                        if items[i].sellIn < 11 {
-                            if items[i].quality < 50 {
-                                items[i].quality = items[i].quality + 1
-                            }
+                    if items[i].sellIn < 11 {
+                        if items[i].quality < 50 {
+                            items[i].quality = items[i].quality + 1
                         }
-                        
-                        if items[i].sellIn < 6 {
-                            if items[i].quality < 50 {
-                                items[i].quality = items[i].quality + 1
-                            }
+                    }
+                    
+                    if items[i].sellIn < 6 {
+                        if items[i].quality < 50 {
+                            items[i].quality = items[i].quality + 1
                         }
                     }
                 }
-            }
-            
-            if items[i].name != "Sulfuras, Hand of Ragnaros" {
                 items[i].sellIn = items[i].sellIn - 1
-            }
-            
-            if items[i].sellIn < 0 {
-                if items[i].name != "Aged Brie" {
-                    if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-                        if items[i].quality > 0 {
-                            if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if items[i].quality < 50 {
-                        items[i].quality = items[i].quality + 1
+                
+                if items[i].sellIn < 0 {
+                    items[i].quality = items[i].quality - items[i].quality
+                }
+                
+            case .sulfuras:
+                break
+            case nil:
+                if items[i].quality > 0 {
+                    items[i].quality = items[i].quality - 1
+                }
+                items[i].sellIn = items[i].sellIn - 1
+                
+                if items[i].sellIn < 0 {
+                    if items[i].quality > 0 {
+                        items[i].quality = items[i].quality - 1
                     }
                 }
             }
@@ -53,4 +53,19 @@ public class GildedRose {
    
         return items
     }
+    
+}
+
+extension Item {
+    mutating func addQuantity(amount: Int = 1) {
+        if self.quality < 50 {
+            self.quality = self.quality + amount
+        }
+    }
+}
+
+enum ItemName: String {
+    case agedBrie = "Aged Brie"
+    case TAFKAL80ETC = "Backstage passes to a TAFKAL80ETC concert"
+    case sulfuras = "Sulfuras, Hand of Ragnaros"
 }
