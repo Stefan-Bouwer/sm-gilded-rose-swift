0f5d016119b9fd011ff77fe9dac5cbfc3c494ff2
Stefan Bouwer <stefan.bouwer@impalastudios.com>
2024-09-06 15:51:50 +0200

    Kata'ing like champions

--- GildedRose/GildedRose.swift
+++ GildedRose/GildedRose.swift
@@ -1,56 +1,106 @@
 public class GildedRose {
-    public static func updateQuality(_ items: Array<Item>) -> [Item] {
-        var items = items
-        for i in 0 ..< items.count {
-            if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-                if items[i].quality > 0 {
-                    if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if items[i].quality < 50 {
-                    items[i].quality = items[i].quality + 1
-                    
-                    if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-                        if items[i].sellIn < 11 {
-                            if items[i].quality < 50 {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                        
-                        if items[i].sellIn < 6 {
-                            if items[i].quality < 50 {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
+    enum Name: String {
+        case brie = "Aged Brie"
+        case backstage = "Backstage passes to a TAFKAL80ETC concert"
+        case sulfuras = "Sulfuras, Hand of Ragnaros"
+        case anything
+    }
+    
+    private static func updateAnything(item: inout Item) {
+        guard item.quality > 0 else {
+            return
+        }
+        
+        item.quality = item.quality - 1
+    }
+    
+    private static func updateBrie(item: inout Item) {
+        guard item.quality < 50 else {
+            return
+        }
+        
+        item.quality = item.quality + 1
+    }
+    
+    private static func updateBackstage(item: inout Item) {
+        guard item.quality < 50 else {
+            return
+        }
+        
+        item.quality = item.quality + 1
+        
+        if item.sellIn < 11 {
+            if item.quality < 50 {
+                item.quality = item.quality + 1
             }
-            
-            if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                items[i].sellIn = items[i].sellIn - 1
+        }
+        
+        if item.sellIn < 6 {
+            if item.quality < 50 {
+                item.quality = item.quality + 1
             }
-            
-            if items[i].sellIn < 0 {
-                if items[i].name != "Aged Brie" {
-                    if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-                        if items[i].quality > 0 {
-                            if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                                items[i].quality = items[i].quality - 1
-                            }
+        }
+    }
+    
+    private static func updatebrieOrBackstage(item: inout Item) {
+        switch item.name {
+        case Name.brie.rawValue:
+            updateBrie(item: &item)
+        case Name.backstage.rawValue:
+            updateBackstage(item: &item)
+        default:
+            break
+        }
+    }
+    
+    private static func updateAllExceptSulfurasSellIn(item: inout Item) {
+        if item.name != Name.sulfuras.rawValue {
+            item.sellIn = item.sellIn - 1
+        }
+    }
+    
+    private static func updateIfSellInLessThanZero(item: inout Item) {
+        if item.sellIn < 0 {
+            if item.name != Name.brie.rawValue {
+                if item.name != Name.backstage.rawValue {
+                    if item.quality > 0 {
+                        if item.name != Name.sulfuras.rawValue {
+                            item.quality = item.quality - 1
                         }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
                     }
                 } else {
-                    if items[i].quality < 50 {
-                        items[i].quality = items[i].quality + 1
-                    }
+                    item.quality = item.quality - item.quality
+                }
+            } else {
+                if item.quality < 50 {
+                    item.quality = item.quality + 1
                 }
             }
         }
+    }
+    
+    public static func updateQuality(_ items: Array<Item>) -> [Item] {
+        var newArray = Array<Item>()
+        
+        for var item in items {
+            switch item.name {
+            case Name.brie.rawValue:
+                updateBrie(item: &item)
+            case Name.backstage.rawValue:
+                updateBackstage(item: &item)
+            case Name.sulfuras.rawValue:
+                break
+            default:
+                updateAnything(item: &item)
+            }
+            
+            updateAllExceptSulfurasSellIn(item: &item)
+            
+            updateIfSellInLessThanZero(item: &item)
+            
+            newArray.append(item)
+        }
    
-        return items
+        return newArray
     }
 }
