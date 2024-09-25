3742e28d30dbf6d51642bd383e5a5877c26c0ce0
Mahdi Mahjoobi <mahdi.mahjoobi@impalastudios.com>
2024-09-25 17:03:47 +0200

    Did best today

--- GildedRose/GildedRose.swift
+++ GildedRose/GildedRose.swift
@@ -1,10 +1,21 @@
 public class GildedRose {
+    
+    enum Products: String {
+        case agedBrie = "Aged Brie"
+        case backstage = "Backstage passes to a TAFKAL80ETC concert"
+        case sulfuras = "Sulfuras, Hand of Ragnaros"
+        case sulfurasConjured = "Sulfuras, Hand of Ragnaros (Conjured)"
+    }
+    
+    
     public static func updateQuality(_ items: Array<Item>) -> [Item] {
         var items = items
         for i in 0 ..< items.count {
-            if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
+            let name = Products(rawValue: items[i].name)
+            
+            if items[i].name != Products.agedBrie.rawValue && items[i].name != Products.backstage.rawValue {
                 if items[i].quality > 0 {
-                    if items[i].name != "Sulfuras, Hand of Ragnaros" {
+                    if items[i].name != Products.sulfuras.rawValue {
                         items[i].quality = items[i].quality - 1
                     }
                 }
@@ -12,7 +23,7 @@ public class GildedRose {
                 if items[i].quality < 50 {
                     items[i].quality = items[i].quality + 1
                     
-                    if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
+                    if items[i].name == Products.backstage.rawValue {
                         if items[i].sellIn < 11 {
                             if items[i].quality < 50 {
                                 items[i].quality = items[i].quality + 1
@@ -28,29 +39,53 @@ public class GildedRose {
                 }
             }
             
-            if items[i].name != "Sulfuras, Hand of Ragnaros" {
+            switch name {
+            case .agedBrie:
+                items[i].sellIn = items[i].sellIn - 1
+            case .backstage:
+                items[i].sellIn = items[i].sellIn - 1
+            case .sulfuras:
+                break
+            case .sulfurasConjured:
+                items[i].sellIn = items[i].sellIn - 1
+            default:
                 items[i].sellIn = items[i].sellIn - 1
             }
-            
-            if items[i].sellIn < 0 {
-                if items[i].name != "Aged Brie" {
-                    if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-                        if items[i].quality > 0 {
-                            if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if items[i].quality < 50 {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
-            }
+
+            checkSellIn(item: &items[i])
         }
    
         return items
+    }
+    
+    private static func checkSellIn(item: inout Item) {
+        guard item.sellIn < 0 else {
+            return
+        }
+        
+        guard item.name != Products.agedBrie.rawValue else {
+            if item.quality < 50 {
+                item.quality = item.quality + 1
+            }
+            return
+        }
+        
+        if item.name != Products.backstage.rawValue {
+            if item.quality > 0 {
+                if item.name != Products.sulfuras.rawValue {
+                    item.quality = item.quality - 1
+                }
+            }
+        } else {
+            item.quality = item.quality - item.quality
+        }
+    }
+    
+    private static func reduceQuantityIfNotSulfuras(item: inout Item) {
+        if item.quality > 0 {
+            if item.name != Products.sulfuras.rawValue {
+                item.quality = item.quality - 1
+            }
+        }
     }
 }
